4b13d30949f909c835b4cb7ec0b15c2e134edd54
anapaulaVS <valezeanapaula123@gmail.com>
2025-04-05 01:36:50 +0000

    commit

--- 01 NOVATO - Batalha Naval/01desafio.c
+++ 01 NOVATO - Batalha Naval/01desafio.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 // Função para imprimir o Tabuleiro
+
 void imprimirTabuleiro(int tabuleiro[10][10]) {
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
@@ -16,13 +17,16 @@ int main() {
     printf(" \n");
 
     // Inicializar o Tabuleiro com '0s' (ZEROS) representando Água
+
     int tabuleiro[10][10] = {0};
      
     // Declarar e inicializar os Vetores para representar os Navios
+
     int navioHorizontal[3] = {3, 3, 3};
     int navioVertical[3] = {3, 3, 3};
 
     // Definir as coordenadas iniciais dos Navios
+
     int coordenadaNavioHorizontal[] = {2, 5}; // linha e coluna
     int coordenadaNavioVertical[] = {7, 3}; // linha e coluna
 
