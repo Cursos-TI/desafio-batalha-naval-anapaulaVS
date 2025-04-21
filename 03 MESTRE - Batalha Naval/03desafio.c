3ca29bde41ce9a57f89ac674b298b3f275e510ef
anapaulaVS <valezeanapaula123@gmail.com>
2025-04-21 01:12:33 +0000

    Definição do tamanho do tabuleiro

--- 03 MESTRE - Batalha Naval/03desafio.c
+++ 03 MESTRE - Batalha Naval/03desafio.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+// Definição do tamanho do tabuleiro (10x10)
+
+#define TAMANHO_TABULEIRO 10
+
+void imprimirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    printf(" A B C D E F G H I J\n");
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        printf("%d ", i + 1);
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
 
-    printf("TABULEIRO BATALHA NAVAL - AVENTUREIRO! \n");
+    printf("TABULEIRO BATALHA NAVAL - MESTRE! \n");
     printf(" \n");
 
 return 0;
